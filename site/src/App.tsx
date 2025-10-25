5d3f4fa2027e1e00d1101ebeffc0c7aeda0baa6d
idksarah <sarah.p.ngai@gmail.com>
2025-10-25 11:52:23 -0700

    tts

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,7 +1,8 @@
-import { createSignal } from 'solid-js'
-import { render } from 'solid-js/web';
+import { createSignal } from 'solid-js';
 import { fetchGPTResponse } from './FetchGPTResponse';
+import textToSpeech  from './TextToSpeech';
 import './App.css'
+import chairman from "../assets/xi-jinping-idle.jpg";
 
 export default function App() {
   const [userInput, setUserInput] = createSignal("");
@@ -17,24 +18,33 @@ export default function App() {
 
     const response = await fetchGPTResponse(userInput());
     setWrapperResponse(response);
+    textToSpeech(response.value);
     setLoading(false);
   }
 
   return (
-    <div class="bg-white shadow-md rounded-lg p-8 m-auto max-w-lg">
-      <textarea
-        value={userInput()}
-        onChange={(ev) => setUserInput(ev.target.value)}
-      ></textarea>
-      <button onClick={getResponse} disabled={loading()}>
-        Get
-      </button>
-      {!loading() && wrapperResponse().value && (
-        <>
-          <p class="bg-gray-100">{wrapperResponse().value}</p>
-        </>
-      )}
-      {/* and a container where the GPT response will be rendered. */}
+    <div id="main">
+      <section id="header">
+        <h2 id="title">centralized child protection</h2>
+      </section>
+      <img src={chairman} id="chairman"></img>
+      <section id="yap">
+        <textarea
+          value={userInput()}
+          onInput={(ev) => setUserInput(ev.target.value)}
+          id="input"
+        ></textarea>
+        <button onClick={getResponse} disabled={loading()}>
+          Get
+        </button>
+      </section>
+      
+        {!loading() && wrapperResponse().value && (
+          <>
+            <p id="wrapperResponse">{wrapperResponse().value}</p>
+          </>
+        )}
+        {/* and a container where the GPT response will be rendered. */}
     </div>
   );
 }
