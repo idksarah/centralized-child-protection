ff2a64735bf7adf885384ace9b97ef01081a136a
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-25 17:42:37 -0700

    Added export files

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid()],build: {
+      outDir: 'docs', 
+    },
 })
